5a08a41991daad7ab5da67b8799d0a23ca3cd730
OlgaMir <tarasovaolvl@rambler.ru>
2022-10-13 16:50:12 +0300

    completed task 27

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -3,9 +3,9 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
-
+/*
 {
-    double MyPow ( int A, int B )
+    double myPow ( int A, int B )
     {
         double result = 1.0; 
         for ( int i = 1; i <= B; ++i )
@@ -24,5 +24,43 @@
     if ( B <= 0 )
         Console.WriteLine ( $"Power {B} is not a natural number." );
     else
-        Console.WriteLine ( MyPow ( A, B ) );
+        Console.WriteLine ( myPow ( A, B ) );
+}
+*/
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+{
+    int [] numberToDigits ( int number )
+    {
+        int[] digits = new int[1 + ( int ) Math.Log10 ( number )];
+        for (int i = digits.Length - 1; i >= 0; --i)
+        {
+            int digit;
+            number = Math.DivRem ( number, 10, out digit );
+            digits[i] = digit;
+        }
+        return digits;
+    }
+
+    int numbersSum ( int [] numbers )
+    {
+        int sum = 0;
+        for ( int i = 0; i < numbers.Length; ++i )
+            sum = sum + numbers [i];
+        return sum;
+    }
+
+    Console.WriteLine ( "Task 27" );
+
+    Console.Write ( "Input a number: " );
+    int number = Convert.ToInt32 ( Console.ReadLine () );
+
+    int digitsSum = numbersSum ( numberToDigits ( number ) );
+
+    Console.WriteLine ( $"Digits sum in number {number} is {digitsSum}." );
 }
