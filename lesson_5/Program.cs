2026c1d3b90b2950afdafeb186f4fe6cdc2b0cf8
OlgaMir <tarasovaolvl@rambler.ru>
2022-10-20 22:36:36 +0300

    Completed task 38 and seminar 5

--- lesson_5/Program.cs
+++ lesson_5/Program.cs
@@ -88,3 +88,67 @@ void outputArray ( int [] arr )
         Console.WriteLine ( $" has summ of odd items equal to {oddsSum}." );
     }
 }
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+{
+    float [] generateFloatArray ( int size, int from_value, int to_value )
+    {
+        Random rnd = new Random ();
+        int range = to_value - from_value;
+
+        float [] arr = new float [size];
+        for ( int i = 0; i < size; ++i )
+            arr [i] = rnd.Next ( range ) + from_value;
+
+        return arr;
+    }
+
+    void outputFloatArray ( float [] arr )
+    {
+        Console.Write ( "[" );
+        
+        for ( int i = 0; i < arr.Length - 1; ++i )
+            Console.Write ( arr [i] + ", " );
+        Console.Write ( arr [arr.Length - 1] );
+
+        Console.Write ( "]" );
+    }
+
+    float maximal ( float [] arr )
+    {
+        float max = arr [0];
+        for ( int i = 1; i < arr.Length; ++i )
+            max = Math.Max ( max, arr [i] );
+        return max;
+    }
+
+    float minimal ( float [] arr )
+    {
+        float min = arr [0];
+        for ( int i = 1; i < arr.Length; ++i )
+            min = Math.Min ( min, arr [i] );
+        return min;
+    }
+
+    Console.WriteLine ( "Task 38" );
+
+    Console.Write ( "Input array length N: " );
+    int N = Convert.ToInt32 ( Console.ReadLine () );
+
+    if ( N <= 0 )
+        Console.WriteLine ( $"Power {N} is not a valid array length." );
+    else
+    {
+        float [] array = generateFloatArray ( N, 0, 100 );
+        outputFloatArray ( array );
+
+        float min_val = minimal ( array );
+        float max_val = maximal ( array );
+        float diff = max_val - min_val;
+        Console.WriteLine ( $" has max = {max_val}, min = {min_val} and max-min = {diff}." );
+    }
+}
