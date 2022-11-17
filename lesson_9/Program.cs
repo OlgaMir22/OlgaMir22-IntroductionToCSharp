6d5c604b76ea4dc76ea50ae26b9707bb48df00e3
OlgaMir <tarasovaolvl@rambler.ru>
2022-11-17 19:50:35 +0300

    Completed task 66

--- lesson_9/Program.cs
+++ lesson_9/Program.cs
@@ -35,3 +35,41 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
         Console.WriteLine ( "\"" );
     }
 }
+
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+{
+    int rangedSum ( int from, int to )
+    {
+        if ( from == to )
+            return to;
+        else
+            return from + rangedSum ( from + 1, to );
+    }
+
+    Console.WriteLine ( "Task 66" );
+
+    Console.Write ( "Input number M: " );
+    int M = Convert.ToInt32 ( Console.ReadLine () );
+
+    Console.Write ( "Input number N: " );
+    int N = Convert.ToInt32 ( Console.ReadLine () );
+
+    if ( M > N )
+    {
+        Console.WriteLine ( $"Number M = {M} should be less then N = {N}. Swapped M and N." );
+        ( M, N ) = ( N, M );
+    }
+
+    if ( M < 1 )
+        Console.WriteLine ( "M should be a natural number." );
+    else
+    {
+        int sum = rangedSum ( M, N );
+        Console.WriteLine ( $"M = {M}; N = {N} -> {sum}" );
+    }
+}
